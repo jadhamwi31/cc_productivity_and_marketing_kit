fe5e51d05e08c7d33348b85ce52ff0f4fd44d70d
BabaGanoush <server.vbs@gmail.com>
2023-10-26 13:13:55 +0300

    Adding Navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,9 @@ import {
 } from 'react-router-dom';
 import Video from './pages/Video/Video';
 import Home from './pages/Home';
+import Main from './layout/Main';
+import Signup from './pages/auth/Signup';
+import Login from './pages/auth/Login';
 
 export default function App() {
   return (
@@ -13,7 +16,11 @@ export default function App() {
       router={createBrowserRouter(
         createRoutesFromElements(
           <>
-            <Route path='/' element={<Home />} />
+            <Route path='/' element={<Main />}>
+              <Route index element={<Home />} />
+              <Route path='login' element={<Login />} />
+              <Route path='signup' element={<Signup />} />
+            </Route>
             <Route path='/video' element={<Video />} />
           </>,
         ),
