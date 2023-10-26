c570372a405cfb5f4f149b72921e78760b2a41f6
Ahmad Abo Elzahab <server.vbs@gmail.com>
2023-10-26 20:04:51 +0300

    adding header

--- client/src/layout/Dashboard.tsx
+++ client/src/layout/Dashboard.tsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from '../components/Dashboard/Navbar';
+import Header from '../components/Dashboard/Header';
 
 export default function Dashboard() {
   return (
     <div className='bg-black h-screen overflow-y-scroll bg_grid pb-36'>
       <Navbar />
-      <div className='p-2 lg:pl-72 '>
-        <div className='mx-auto max-w-8xl space-y-8 px-2 pt-20 lg:px-0 lg:py-8 text-white'>
+      <div className=' lg:pl-60 '>
+        <div className='mx-auto max-w-8xl space-y-8 px-2 pt-20 lg:pt-10  text-white'>
+          <Header />
           <Outlet />
         </div>
       </div>
