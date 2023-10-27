2784e9ace0454cbb0a1761c6e2c507441da191aa
Ahmad Abo Elzahab <server.vbs@gmail.com>
2023-10-27 14:02:50 +0200

    favico

--- client/src/components/Dashboard/Navbar.tsx
+++ client/src/components/Dashboard/Navbar.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import 
+import Logo from '../Main/Logo';
 import { MdOutlineDashboard } from 'react-icons/md';
 import { RiSettings4Line } from 'react-icons/ri';
 import { TbReportAnalytics } from 'react-icons/tb';
@@ -21,11 +21,7 @@ const Navbar = () => {
 
   return (
     <section className='flex   gap-6'>
-      <div
-        className='fixed top-0 left-0 h-screen bg-New_Gray 
-          w-16
-      text-gray-100 px-4'
-      >
+      <div className='fixed top-0 left-0 h-screen bg-New_Gray w-16 text-gray-100 px-4'>
         <div className='mt-4 flex flex-col gap-4 relative'>
           {menus?.map((menu, i) => (
             <Link
@@ -36,11 +32,7 @@ const Navbar = () => {
               } group flex items-center text-sm  gap-3.5 font-medium p-2 hover:bg-gray-800 rounded-md`}
             >
               <div>{React.createElement(menu?.icon, { size: '20' })}</div>
-
-              <h2
-                className='
-                 absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit '
-              >
+              <h2 className='absolute left-48 bg-white font-semibold whitespace-pre text-gray-900 rounded-md drop-shadow-lg px-0 py-0 w-0 overflow-hidden group-hover:px-2 group-hover:py-1 group-hover:left-14 group-hover:duration-300 group-hover:w-fit '>
                 {menu?.name}
               </h2>
             </Link>
