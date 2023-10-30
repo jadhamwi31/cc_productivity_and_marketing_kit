83d02150e0796d0f84e9a77acc5da863ab7d7d64
Ahmad Abo Elzahab <server.vbs@gmail.com>
2023-10-30 17:38:31 +0300

    an update

--- client/src/pages/Dashboard/GraphicEditor.tsx
+++ client/src/pages/Dashboard/GraphicEditor.tsx
@@ -5,21 +5,31 @@ import React, { useState, useEffect } from 'react';
 import { BsType } from 'react-icons/bs';
 import { TbPhotoSearch } from 'react-icons/tb';
 import { IoShapesOutline } from 'react-icons/io5';
-import { AiOutlineZoomIn, AiOutlineZoomOut } from 'react-icons/ai';
+import { BiColorFill } from 'react-icons/bi';
+import {
+  AiOutlineZoomIn,
+  AiOutlineColumnHeight,
+  AiOutlineColumnWidth,
+  AiOutlineZoomOut,
+} from 'react-icons/ai';
+import { FiSave } from 'react-icons/fi';
 
 import Shapes from '../../components/Dashboard/GraphicEditor/Shapes';
 
 export default function GraphicEditor() {
   //Background
-  const [width, setWidth] = useState(1080);
-  const [height, setHeight] = useState(1080);
+  const [width, setWidth] = useState(800);
+  const [height, setHeight] = useState(800);
   const [zoom, setZoom] = useState(70);
   const [backgroundColor, setBackgroundColor] = useState('#eeeeee');
 
   return (
     <div className='flex justify-between'>
       <div className=' h-[95vh] w-[5vw] flex flex-col items-center justify-center'>
-        <div className=' bg-[#2a2438] justify-around rounded-lg  flex flex-col text-white'>
+        <button className='px-2 py-2 bg-[#2a2438] hover:bg-[#4f245f] rounded-lg'>
+          <FiSave size='25' />
+        </button>
+        <div className=' bg-[#2a2438] justify-around rounded-lg  flex flex-col text-white mt-10'>
           <button className='px-2 py-1 hover:bg-[#4f245f] hover:rounded-t-lg '>
             <BsType size='25' />
           </button>
@@ -33,7 +43,7 @@ export default function GraphicEditor() {
         <div className=' justify-around flex flex-col text-white mt-10'>
           <button
             className='px-2 py-2 bg-[#2a2438] hover:bg-[#4f245f] rounded-lg  '
-            onClick={() => setZoom((prevZoom) => prevZoom + 10)}
+            onClick={() => setZoom((prevZoom) => prevZoom + 5)}
           >
             <AiOutlineZoomIn size='25' />
           </button>
@@ -41,7 +51,7 @@ export default function GraphicEditor() {
 
           <button
             className='px-2 py-2 bg-[#2a2438] hover:bg-[#4f245f] rounded-lg  '
-            onClick={() => setZoom((prevZoom) => prevZoom - 10)}
+            onClick={() => setZoom((prevZoom) => prevZoom - 5)}
           >
             <AiOutlineZoomOut size='25' />
           </button>
@@ -63,14 +73,21 @@ export default function GraphicEditor() {
         </Stage>
       </div>
       <div className='h-[95vh] mr-16'>
-        <div className='rounded-lg bg-black border-[1px] border-New_Gray p-4 text-black'>
-          <div className='flex flex-col justify-center text-white'>
-            width
-            <input type='text' className='rounded px-2 py-1 bg-[#0e0e0e]' />
-            height
-            <input type='text' className='rounded px-2 py-1 bg-gray-700 ' />
-            Background Color
-            <HexColorPicker color={backgroundColor} onChange={setBackgroundColor} />;
+        <div className='rounded-lg bg-[#15121c] border-[1px] border-New_Gray p-4 text-black'>
+          <div className='flex flex-col justify-center text-gray-500'>
+            <label htmlFor='' className='flex'>
+              <AiOutlineColumnWidth size='22' className='mr-2' /> Width
+            </label>
+            <input type='text' className='rounded px-2 py-1 bg-[#2a2438] mb-4' />
+            <label htmlFor='' className='flex'>
+              <AiOutlineColumnHeight size='22' className='mr-2' /> Height
+            </label>{' '}
+            <input type='text' className='rounded px-2 py-1 bg-[#2a2438]  mb-4' />
+            <label htmlFor='' className='flex'>
+              <BiColorFill size='22' className='mr-2' />
+              Background Color
+            </label>
+            <HexColorPicker color={backgroundColor} onChange={setBackgroundColor} />
           </div>
         </div>
       </div>
