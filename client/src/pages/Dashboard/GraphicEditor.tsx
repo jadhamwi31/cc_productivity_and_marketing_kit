ea742aa8d3b236a97388a1085171e29095210659
Ahmad Abo Elzahab <server.vbs@gmail.com>
2023-10-30 12:43:03 +0300

    add react-konva

--- client/src/pages/Dashboard/GraphicEditor.tsx
+++ client/src/pages/Dashboard/GraphicEditor.tsx
@@ -1,18 +1,16 @@
 import Shapes from '../../components/Dashboard/GraphicEditor/Shapes';
+import React, { useState, useEffect } from 'react';
+import { Stage, Layer, Text, Image, Rect } from 'react-konva';
+
 
 export default function GraphicEditor() {
   return (
     <div className='flex justify-between'>
       <div>
-
         <Shapes />
       </div>
+      <div className='flex   items-center'></div>
       <div>
-
-        <Shapes />
-      </div>
-      <div>
-
         <Shapes />
       </div>
     </div>
