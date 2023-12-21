d8f15975d36a0590b96e3f9742d87df5458e7126
Ahmad Abo Elzahab <server.vbs@gmail.com>
2023-12-21 20:45:33 +0300

    finsihng sign up

--- client/src/pages/auth/Signup.tsx
+++ client/src/pages/auth/Signup.tsx
@@ -1,48 +1,129 @@
 import { Link } from 'react-router-dom';
 import building from '../../assets/artworks/building.svg';
-import yard from '../../assets/artworks/yard.svg';
-import mountain from '../../assets/artworks/mountain.svg';
+
+import { useState } from 'react';
+import { axios } from '../../lib/axios';
+import { AxiosResponse } from 'axios';
+import { toast } from 'react-toastify';
+
 export default function Signup() {
-  const images = [building, yard, mountain];
-  const randomImage = images[Math.floor(Math.random() * images.length)];
-  console.log(randomImage);
+  const [email, setEmail] = useState<string>('');
+  const [userName, setUserName] = useState<string>('');
+  const [firstName, setFirstName] = useState<string>('');
+  const [lastName, setLastName] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [confirmPassword, setConfirmPassword] = useState<string>('');
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (password != confirmPassword) {
+      toast.error('Passowrds not Matchinchg');
+    } else {
+      try {
+        const response: AxiosResponse = await axios.post('/auth/signup', {
+          username: userName,
+          password: password,
+          firstname: firstName,
+          lastname: lastName,
+          email,
+        });
+        setEmail('');
+        setUserName('');
+        setFirstName('');
+        setLastName('');
+        setConfirmPassword('');
+        setPassword('');
+        toast.success('New Account has been Created!');
+      } catch (error: any) {
+        if (error.response && error.response.data && error.response.data.errors) {
+          const firstError = error.response.data.errors[0];
+          const errorMessage = firstError.message || 'An error occurred';
+        } else {
+        }
+      }
+    }
+  };
+
   return (
-    <div className=' lg:flex lg:justify-between w-screen  h-screen '>
+    <div className='lg:flex lg:justify-between w-screen h-screen '>
       <div
         className='absolute h-screen w-screen bg-black/60 backdrop-blur-md lg:w-[30vw] z-20
                   lg:bg-transparent lg:flex p-20 items-center justify-center
                   lg:static'
       >
-        <form className='flex-col  '>
+        <form className='flex-col' onSubmit={handleSubmit}>
+          <p className='text-gray-400 text-l mb-20 '>
+            <span className='text-white font-bold'> Welcome to CreatorsYard!</span> Exciting
+            adventures await as you join our vibrant community. Let's get started and create
+            something amazing together!
+          </p>
           <label htmlFor='' className='text-gray-200 text-md  '>
             Username
           </label>
-
           <input
+            required
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
             type='text'
-            className='bg-[#262626] mb-4 appearance-none  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+            className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#262626] focus:border-2 border-2  border-[#262626] focus:border-purple-500'
           />
+          <div className='flex'>
+            <div className='flex flex-col mr-2  w-full'>
+              <label htmlFor='' className='text-gray-200 text-md  '>
+                First Name
+              </label>
+              <input
+                required
+                value={firstName}
+                onChange={(e) => setFirstName(e.target.value)}
+                type='text'
+                className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#262626] focus:border-2 border-2  border-[#262626] focus:border-purple-500'
+              />
+            </div>
+            <div className='flex flex-col w-full'>
+              <label htmlFor='' className='text-gray-200 text-md  '>
+                Last Name
+              </label>
+              <input
+                required
+                value={lastName}
+                onChange={(e) => setLastName(e.target.value)}
+                type='text'
+                className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#262626] focus:border-2 border-2  border-[#262626] focus:border-purple-500'
+              />
+            </div>
+          </div>
+
           <label htmlFor='' className='text-gray-200 text-md  '>
             Email
           </label>
 
           <input
-            type='text'
-            className='bg-[#262626] mb-4 appearance-none  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+            required
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            type='email'
+            className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#262626] focus:border-2 border-2  border-[#262626] focus:border-purple-500'
           />
           <label htmlFor='' className='text-gray-200 text-md '>
             Password
           </label>
           <input
+            required
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
             type='password'
-            className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+            className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#262626] focus:border-2 border-2  border-[#262626] focus:border-purple-500'
           />
           <label htmlFor='' className='text-gray-200 text-md '>
             Confirm Password
           </label>
           <input
+            required
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
             type='password'
-            className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500'
+            className='bg-[#262626] appearance-none mb-4  rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-[#262626] focus:border-2 border-2  border-[#262626] focus:border-purple-500'
           />
           <button className='bg-purple-900 text-[#A149FA] mb-4  hover:text-white py-2 rounded  w-full text-md'>
             Sign up
@@ -50,7 +131,7 @@ export default function Signup() {
 
           <Link to='/login'>
             <p className='text-gray-500   ml-auto w-full text-right hover:underline hover:text-white'>
-              Already have account
+              Already have an account
             </p>
           </Link>
         </form>
@@ -59,14 +140,14 @@ export default function Signup() {
         <div
           className='lg:absolute w-screen lg:w-[70vw] h-screen bg-cover lg:z-10'
           style={{
-            backgroundImage: `url(${randomImage})`,
+            backgroundImage: `url(${building})`,
           }}
         ></div>
 
         <div
           className='lg:block hidden w-screen lg:w-[70vw] h-screen bg-cover blur-xl'
           style={{
-            backgroundImage: `url(${randomImage})`,
+            backgroundImage: `url(${building})`,
           }}
         ></div>
       </div>
