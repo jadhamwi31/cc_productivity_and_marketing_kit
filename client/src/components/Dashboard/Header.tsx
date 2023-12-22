05474c9357dc20d766f2f22252e10d301ceef3be
Ahmad Abo Elzahab <server.vbs@gmail.com>
2023-12-22 17:52:41 +0300

    unxxx

--- client/src/components/Dashboard/Header.tsx
+++ client/src/components/Dashboard/Header.tsx
@@ -1,3 +1,3 @@
 export default function Header() {
-  return <div className=' h-[5vh] w-[90vw] block ml-auto mr-auto bg-red'></div>;
+  return <div className=' h-[5vh] w-[90vw] block ml-auto mr-auto'></div>;
 }
