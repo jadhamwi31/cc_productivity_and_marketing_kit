03304c50c1ad0f29ec0096dc5c88abbff355e397
Ahmad Abo Elzahab <server.vbs@gmail.com>
2024-01-19 15:09:50 +0300

    youtube videos

--- scrapers/src/controllers/Youtube.controller.ts
+++ scrapers/src/controllers/Youtube.controller.ts
@@ -1,6 +1,9 @@
 import { Request, Response } from 'express';
-import puppeteer from 'puppeteer';
+import * as puppeteer from 'puppeteer';
 import STATUS_CODES from 'http-status-codes';
+async function timeout(ms: number): Promise<void> {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 const channelInfo = async (req: Request<{}, {}, { url: string }>, res: Response) => {
   const { url } = req.body;
@@ -8,14 +11,12 @@ const channelInfo = async (req: Request<{}, {}, { url: string }>, res: Response)
     const browser = await puppeteer.launch({ headless: 'new' });
     const page = await browser.newPage();
     await page.goto(`${url}`, { timeout: 0 });
-
     const data = await page.evaluate(() => {
       const avatarElement = document.querySelector('#img');
       const channelNameElement = document.title;
       const channelHandleElement = document.querySelector('#channel-handle');
       const subscriberCountElement = document.querySelector('#subscriber-count');
       const videosCountElement = document.querySelector('#videos-count');
-
       if (
         !avatarElement ||
         !channelNameElement ||
@@ -28,7 +29,6 @@ const channelInfo = async (req: Request<{}, {}, { url: string }>, res: Response)
           message: 'Error: Required elements not found on the page.',
         });
       }
-
       const avatar = avatarElement.getAttribute('src');
       const channelName = channelNameElement.split(' - YouTube')[0];
       const channelHandle = channelHandleElement.textContent;
@@ -67,5 +67,79 @@ const channelInfo = async (req: Request<{}, {}, { url: string }>, res: Response)
       .send({ status: STATUS_CODES.BAD_REQUEST, message: e });
   }
 };
+const channelVideos = async (req: Request<{}, {}, { url: string }>, res: Response) => {
+  const { url } = req.body;
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
+  try {
+    await page.goto(url, { timeout: 0 });
 
-export const YoutubeController = { channelInfo };
+    await page.evaluate(() => {
+      (document.body.style as any).zoom = '0.1';
+    });
+
+    let previousHeight;
+    while (true) {
+      const currentHeight = await page.evaluate('document.documentElement.scrollHeight');
+
+      if (previousHeight && currentHeight === previousHeight) {
+        break;
+      }
+
+      await page.evaluate('window.scrollTo(0, document.documentElement.scrollHeight)');
+      previousHeight = currentHeight;
+      await timeout(3000);
+    }
+
+    const videoDetails = await page.evaluate(async () => {
+      const details = [];
+      const videoElements = document.querySelectorAll('ytd-rich-grid-media');
+
+      for (const videoElement of videoElements) {
+        const anchorElement = videoElement.querySelector('a#thumbnail');
+        const href = anchorElement
+          ? 'https://www.youtube.com' + anchorElement.getAttribute('href')
+          : 'N/A';
+
+        const thumbnailElement = videoElement.querySelector('img');
+        const thumbnailUrl = thumbnailElement ? thumbnailElement.getAttribute('src') : 'N/A';
+
+        const isRelativeUrl = thumbnailUrl && thumbnailUrl.startsWith('/');
+        const absoluteThumbnailUrl = isRelativeUrl
+          ? `https://www.youtube.com${thumbnailUrl}`
+          : thumbnailUrl;
+
+        const titleElement = videoElement.querySelector('yt-formatted-string#video-title');
+        const title = titleElement ? (titleElement as HTMLElement).innerText.trim() : 'N/A';
+
+        const viewsElement = videoElement.querySelector('span.style-scope.ytd-video-meta-block');
+        const views = viewsElement ? (viewsElement as HTMLElement).innerText.trim() : 'N/A';
+
+        const dateElement = videoElement.querySelectorAll(
+          'span.inline-metadata-item.style-scope.ytd-video-meta-block',
+        )[1];
+        const date = dateElement ? (dateElement as HTMLElement).innerText.trim() : 'N/A';
+
+        const durationElement = videoElement.querySelector(
+          'ytd-thumbnail-overlay-time-status-renderer span#text',
+        );
+        const duration = durationElement
+          ? (durationElement as HTMLElement).innerText.trim()
+          : 'N/A';
+
+        details.push({ href, thumbnailUrl: absoluteThumbnailUrl, title, views, date, duration });
+      }
+
+      return details;
+    });
+    await browser.close();
+    return res.status(STATUS_CODES.OK).send({ status: STATUS_CODES.OK, message: videoDetails });
+  } catch (e) {
+    await browser.close();
+    console.error('Error:', e);
+    return res
+      .status(STATUS_CODES.BAD_REQUEST)
+      .send({ status: STATUS_CODES.BAD_REQUEST, message: e });
+  }
+};
+export const YoutubeController = { channelInfo, channelVideos };
--- scrapers/src/routers/Youtube.route.ts
+++ scrapers/src/routers/Youtube.route.ts
@@ -3,4 +3,5 @@ import { YoutubeController } from '../controllers/Youtube.controller';
 export const YoutubeRouter = Router();
 
 YoutubeRouter.post('/channel', YoutubeController.channelInfo);
-YoutubeRouter.post('/videos', YoutubeController.channelInfo);
+YoutubeRouter.post('/videos', YoutubeController.channelVideos
+);
