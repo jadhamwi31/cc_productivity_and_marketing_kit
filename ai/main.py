34830f0182d8b4b8fccf6ed10eeb080e6f41233c
Jad <j.hamwi@centroidsol.com>
2024-01-21 22:08:29 +0300

    reactions api: complete

--- ai/main.py
+++ ai/main.py
@@ -1,14 +1,19 @@
 
+import random
+from pydantic import BaseModel
 from transformers import pipeline
 from fastapi import FastAPI,File, UploadFile
 from fastapi.responses import JSONResponse
+from typing import List
 
-pipe = pipeline(
+transcriptPipeline = pipeline(
     "automatic-speech-recognition",
     model="Subcold/whisper-small-preprocessed-en",chunk_length_s=30
 )
-
-
+reactionsPipeline = pipeline(
+    model="lxyuan/distilbert-base-multilingual-cased-sentiments-student", 
+    return_all_scores=True
+)
   
 app = FastAPI()
 
@@ -17,8 +22,55 @@ app = FastAPI()
 async def transcript(file: UploadFile):
     try:
         fileObject = await file.read()
-        result = pipe(fileObject,batch_size=8,return_timestamps=True)
+        result = transcriptPipeline(fileObject,batch_size=8,return_timestamps=True)
         print(result)
         return JSONResponse(content=result, status_code=200)
+    except Exception as e: 
+        return JSONResponse(content={"error": str(e)}, status_code=500)
+    
+class Comments(BaseModel):
+    comments:List[str]
+
+def get_reactions_for_comment(comment: str):
+    # Model Run
+    
+    [[positive,neutral,negative]] = reactionsPipeline(comment)
+
+    return {
+        "positive": positive["score"],
+        "neutral": neutral["score"],
+        "negative": negative["score"]
+    }
+
+
+def calculateReactionsAverage(comments: List[str]):
+    total_positive = 0
+    total_neutral = 0
+    total_negative = 0
+
+    for comment in comments:
+        reactions = get_reactions_for_comment(comment)
+        total_positive += reactions["positive"]
+        total_neutral += reactions["neutral"]
+        total_negative += reactions["negative"]
+
+    num_comments = len(comments)
+    average_positive = total_positive / num_comments
+    average_neutral = total_neutral / num_comments
+    average_negative = total_negative / num_comments
+
+    return {
+        "positive": average_positive,
+        "neutral": average_neutral,
+        "negative": average_negative
+    }
+
+@app.post("/reactions")
+async def reactions(body: Comments):
+    try:
+        comments = body.comments
+        average = calculateReactionsAverage(comments)
+    
+        return JSONResponse(content=average, status_code=200)
     except Exception as e: 
         return JSONResponse(content={"error": str(e)}, status_code=500)
